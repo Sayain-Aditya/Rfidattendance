0e04491994912f701ab457599036223af925b1e7
Sayain-Aditya <as4316664@gmail.com>
2025-12-17 22:11:54 +0530

    sd

--- config/db.js
+++ config/db.js
@@ -2,13 +2,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ Database Error:", err.message);
-    process.exit(1);
+    throw err;
   }
 };
 
